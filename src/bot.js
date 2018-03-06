8b0da08fb628d79dd779d20b4b4025f51d5c1428
Awlex <wieseralex580@gmail.com>
2018-03-06 22:14:54 +0100

    Fuck you heroku

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,17 @@ import {commands} from "./commands"
 import config from "../src/config"
 import Firebase from "firebase"
 
+var express = require('express')
+var app = express()
+var server = require('http').createServer(app)
+var io = require('socket.io')(server)
+
+app.get('/', function (req, res, next) {
+	res.send('Fuck you, Heroku  ')
+})
+
+server.listen(process.env.PORT || 5000)
+
 Firebase.initializeApp({
 	apiKey: "apiKeyAIzaSyBQJ21osUTMhYwg2LPNejwaeFdxLBO_ibI",
 	authDomain: "wao-bot.firebaseapp.com",

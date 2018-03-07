f34be205fbc4ef5060b8ff5f3180f726ce1ec5dc
Awlex <wieseralex580@gmail.com>
2018-03-07 15:18:12 +0100

    procfile

--- src/bot.js
+++ src/bot.js
@@ -3,25 +3,24 @@ import Discord from "discord.js"
 import {commands} from "./commands"
 import config from "../src/config"
 import Firebase from "firebase"
-
+/*
 var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 
 app.get('/', function (req, res, next) {
-	res.send('Fuck you, Heroku  ')
+	res.send('Fuck you, Heroku')
 })
 
 server.listen(process.env.PORT || 5000)
-
+*/
 Firebase.initializeApp({
 	apiKey: "apiKeyAIzaSyBQJ21osUTMhYwg2LPNejwaeFdxLBO_ibI",
 	authDomain: "wao-bot.firebaseapp.com",
 	databaseURL: "https://wao-bot.firebaseio.com/",
 })
 
-const database = Firebase.database()
 const client = new Discord.Client()
 
 client.on('ready', () => {
